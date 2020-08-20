0e90aaf6e343d53344d04470629d5d20c8ea2849
soujanya <soujanyarbhat27@gmail.com>
2020-08-20 14:10:40 -0700

    added smooth transition

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-$(function () {
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
+$(function() {
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 10) {
             $('.navbar').addClass('below');
         } else {
             $('.navbar').removeClass('below');
@@ -9,13 +9,31 @@ $(function () {
     });
 });
 
+var $root = $('html, body');
 
 $(document).ready(function() {
     "use strict";
-  
+
     $('ul.navbar-nav > li').click(function(e) {
-      $('ul.navbar-nav > li').removeClass('active');
-      $(this).addClass('active');
+        $('ul.navbar-nav > li').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('nav a[href^="#"]').click(function() {
+        var href = $.attr(this, 'href');
+
+        $root.animate({
+            scrollTop: $(this.hash).offset().top
+        }, 500, function() {
+            window.location.hash = href;
+        });
+
+        return false;
+    });
+
+    $('#interests').on('shown.bs.collapse', function() {
+        $root.animate({
+            scrollTop: $(".more").offset().top
+        }, 'slow');
     });
 });
-  
